39caa032c8980d431607799cc02c3abe4b19b9df
Victor Charpenay <victor.charpenay@emse.fr>
2023-11-23 16:00:46 +0100

    misc: document binding

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
-# ROS Binding for W3C WoT
+# Hypermedea ROS Binding
 
-Implementation of a ROS binding for Thing Descriptions, as specified in the W3C [Web of Things (WoT) Binding Templates](https://w3c.github.io/wot-binding-templates/) standard. The targeted version is ROS 1 (ROS 2 not supported).
+Implementation of a ROS binding for [Hypermedea](https://hypermedea.github.io/).
+The targeted version is ROS 1 (ROS 2 not supported).
 
-The current implementation adheres to the interface defined in [`wot-td-java`](https://github.com/Interactions-HSG/wot-td-java) and uses [`jrosbridge`](https://github.com/rctoris/jrosbridge) for ROS communication over WebSockets (rosbridge).
+The current implementation uses [`jrosbridge`](https://github.com/rctoris/jrosbridge) for ROS communication over WebSockets (rosbridge).
--- src/main/java/org/hypermedea/ros/ROS.java
+++ src/main/java/org/hypermedea/ros/ROS.java
@@ -2,25 +2,29 @@ package org.hypermedea.ros;
 
 /**
  * <p>
- *   Vocabulary to declare ROS form attributes in W3C Thing Description documents, as described in the
- *   <a href="https://w3c.github.io/wot-binding-templates/">W3C Web of Things (WoT) Binding Templates</a>
- *   specification.
+ *   Vocabulary to declare ROS form fields in Hypermedea operations.
  * </p>
  * <p>
  *   Example for basic publish/subscribe, where the path of the target URI corresponds to a topic name:
  *   <code><pre>
- * [] a hctl:Form ;
- *    hctl:target "ros+ws://example.org/turtlesim/cmd_vel" ;
- *    ros:messageType "geometry_msgs/Twist" .
- *   </pre></code>
+ns("https://github.com/RobotWebTools/rosbridge_suite/blob/ros1/ROSBRIDGE_PROTOCOL.md#") .
+
++!call_cmdvel : ns(ROS) <-
+  .concat(ROS, "messageType", MessageType) ;
+  put(
+    "ros+ws://example.org/turtlesim/cmd_vel",
+    json([kv("linear", ...), kv("angular", ...)]),
+    [kv(MessageType, "geometry_msgs/Twist")]
+  ) .</pre></code>
  * </p>
  * <p>
  *   Example for <code>actionlib</code> operations, where the path of the target URI corresponds to an action name
  *   and the URI fragment corresponds to the ID of an on-going action:
- *   <code><pre>
- * [] a hctl:Form ;
- *    hctl:target "ros+ws://example.org/turtle_shape#{id}" .
- *   </pre></code>
+ *   <code><pre>+!call_shape : ns(ROS) <-
+  .concat(ROS, "goalId", GoalId) ;
+  post("ros+ws://example.org/turtle_shape#{id}") ;
+  .wait({ +rdf("ros+ws://example.org/turtle_shape", GoalId, ID) }) ;
+ .print(ID) .</pre></code>
  * </p>
  */
 public class ROS {
